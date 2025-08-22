453684a5751f82ef7d4c9b4fb7cb37ccd967ee5f
Aman Kumar <akt9802@gmail.com>
2025-08-22 21:18:28 +0530

    user signup/login/auth and json implemented

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,8 @@
-import express, { json } from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
+import express, { json } from "express";
+import cors from "cors";
+import helmet from "helmet";
 
+import userRoutes from "./routes/user.route.js"
 
 const app = express();
 
@@ -9,12 +10,13 @@ app.use(helmet());
 app.use(cors());
 app.use(json());
 
+app.get("/", (req, res) => res.json({ status: "ok" }));
 
-app.get('/health', (req, res) => res.json({ status: 'ok' }));
+app.use('/api/v1/user',userRoutes);
 
 app.use((err, req, res, next) => {
   console.error(err);
-  res.status(err.status || 500).json({ error: err.message || 'Server error' });
+  res.status(err.status || 500).json({ error: err.message || "Server error" });
 });
 
 export default app;
